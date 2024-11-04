e4c625a600c6f04d82999160a47c6ce4e00d633d
Marco <marc0x71@yahoo.it>
2024-11-04 12:07:24 +0100

    feat: ctest improvement
    Added function to:
    
    - Retrieving test folders
    - Retrieving test list

--- lua/cmake-simple/ctest.lua
+++ lua/cmake-simple/ctest.lua
@@ -1,9 +1,12 @@
 local utils = require('cmake-simple.lib.utils')
+local ntf = require('cmake-simple.lib.notification')
+
+local scandir = require("plenary.scandir")
 
 local ctest = {}
 
 function ctest:new(o)
-  o = o or {preset_list = {}, preset = nil, test_list = {}}
+  o = o or {preset_list = {}, preset = nil, test_dir = nil, test_list = {}, test_folders = {}}
   setmetatable(o, self)
   self.__index = self
   return o
@@ -31,37 +34,80 @@ function ctest:load_presets()
   end
 end
 
+local function _decode_testlist(json)
+  local result = {}
+  for _, test in ipairs(json.tests) do
+    local cwd = utils.get_path(test.command[1])
+    for _, prop in ipairs(test.properties) do
+      if prop.name == 'WORKING_DIRECTORY' then
+        cwd = prop.value
+        break
+      end
+    end
+    result[test.name] = {source = '', line = 0, command = test.command, cwd = cwd, status = 'unk', output = nil}
+  end
+  return result
+end
+
 function ctest:show_testcases()
+  self:search_test_folders()
+  if self.test_dir==nil or next(self.test_folders)==nil then
+    -- something went wrong
+    return
+  end
+
   self.test_list = {}
-  local cmd = {"ctest", "--show-only=json-v1"}
+  local cmd = {"ctest", "--show-only=json-v1", "--test-dir", self.test_dir}
   if next(self.preset_list) ~= nil then
     if self.preset == nil then
       vim.ui.select(self.preset_list, {prompt = "Select preset"}, function(select) self.selected_preset = select end)
     end
     vim.list_extend(cmd, {'--preset', self.selected_preset})
   end
+
   local result = vim.system(cmd, {text = true}):wait()
   if result.code == 0 then
     local json = vim.json.decode(result.stdout)
-    for _, test in ipairs(json.tests) do
-      local cwd = utils.get_path(test.command[1])
-      for _, prop in ipairs(test.properties) do
-        if prop.name == 'WORKING_DIRECTORY' then
-          cwd = prop.value
-          break
-        end
-      end
-      self.test_list[test.name]={
-        source = '',
-        line = 0,
-        command = test.command,
-        cwd = cwd,
-        status = 'unk',
-        output = nil
-      }
-    end
+    self.test_list = _decode_testlist(json)
+    if next(self.test_list) ~= nil then self:search_test_files() end
   end
+  if result.code~=0 or next(self.test_list) == nil then
+    ntf.notify("Failed to locate retrieve test list", vim.log.levels.ERROR)
+    return
+  end
+
+  P(self)
+
+  -- TODO show testcases window
 end
 
+function ctest:search_test_files()
+  -- TODO
+end
+
+function ctest:search_test_folders()
+  self.test_folders = {}
+  self.test_dir = nil
+
+  local cwd = vim.loop.cwd()
+  local folders = scandir.scan_dir(cwd, {
+    respect_gitignore = false,
+    depth = 4,
+    search_pattern = "CTestTestfile.cmake",
+    silent = true
+  })
+  if next(folders) == nil then
+    ntf.notify("Failed to locate CTestTestfile", vim.log.levels.WARN)
+    return
+  end
+  self.test_dir = utils.get_path(folders[1])
+
+  for _, filename in ipairs(folders) do
+    local content = utils.read_all(filename) or ''
+    local start = string.match(content, [[.*Source directory: ([^%s]+).*]], 0)
+    if start ~= nil then vim.list_extend(self.test_folders, {start}) end
+  end
+  if next(self.test_folders) == nil then ntf.notify("Failed to locate tests folders", vim.log.levels.WARN) end
+end
 
 return ctest
